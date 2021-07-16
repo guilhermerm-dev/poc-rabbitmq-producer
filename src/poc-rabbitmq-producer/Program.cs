c0063ab54f091a2d81c2dcfad98559dc9fad9c5a
Guilherme Ramos Mendes <guilherme.rmendes95@gmai.com>
2021-07-15 22:07:04 -0300

    Refactoring folders

---
+++ src/poc-rabbitmq-producer/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace poc_rabbitmq_producer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("****Sending Messages");
+            var rabbitMqClient = new RabbitMqClient();
+
+            for (int i = 0; i < 100000; i++)
+            {
+                var message = string.Concat($"Olá, mundo! nº {i} - {DateTime.Now}");
+                rabbitMqClient.Publish(message);
+                Console.WindowLine("Message Sended with success");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
